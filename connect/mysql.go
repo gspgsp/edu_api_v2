ce11d5d464df5792e3e081e66853b58647e00b8b
guoshipeng <2249875980@qq.com>
2020-07-24 17:22:47 +0800

    mysql time out

--- connect/mysql.go
+++ connect/mysql.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"time"
 )
 
 var Db *sqlx.DB
@@ -12,6 +13,7 @@ var Db *sqlx.DB
 func init() {
 	dataSourceName := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", config.Config.Mysql.DbUsername, config.Config.Mysql.DbPassword, config.Config.Mysql.DbHost, config.Config.Mysql.DbPort, config.Config.Mysql.DbDatabase)
 	db, err := sqlx.Connect("mysql", dataSourceName)
+	db.SetConnMaxLifetime(300 * time.Second)
 
 	if err == nil && db != nil {
 		Db = db
